11642b3a0d8cde9c8fcf1f72ce25b332308f1af6
Basquiat <ememdaniel98@gmail.com>
2021-10-11 17:22:08 -0500

    added array of questions for read me file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt(
+    [{
+            type: 'input',
+            message: 'what is your name?',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'description',
+            name: 'description'
+        },
+        {
+            type: 'checkbox',
+            message: 'table of contents',
+            name: 'table of contents',
+            choices: ['title', 'description', 'table of contents', 'installation', 'license', 'contributing', 'tests', 'questions']
+        },
+        {
+            type: 'input',
+            message: 'installation instructions',
+            name: 'installation'
+        },
+        {
+            type: 'list',
+            message: 'What license are you using?',
+            name: 'license',
+            choices: ['CCO', 'MIT', 'apache', 'MPL', 'GPL', 'AGPL', 'JRL', 'AFPL', 'Proprietary license', 'Trade secrets']
+        },
+        {
+            type: 'input',
+            message: 'contributing',
+            name: 'contributing'
+        },
+        {
+            type: 'input',
+            message: 'tests',
+            name: 'tests'
+        },
+        {
+            type: 'input',
+            message: 'what is your github url?',
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: 'what is your email?',
+            name: 'email'
+        }
+    ]
+)
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
