ccdafa98a7309871254c95c45b17b51fe84ca8fd
Basquiat <ememdaniel98@gmail.com>
2021-10-12 09:46:35 -0500

    passed data around, and now it is generating a read me file

--- Develop/index.js
+++ Develop/index.js
@@ -1,78 +1,81 @@
-// TODO: Include packages needed for this application
+////////////////////////////////////////////////////////////////////
+
+//required packages//
+
+///////////////////////////////////////////////////////////////////
+
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkDown = require('./utils/generateMarkdown')
+const generateMarkDown = require('./utils/generateMarkdown');
 
-console.log(generateMarkDown.color);
-// TODO: Create an array of questions for user input
-inquirer.prompt(
-    [{
-            type: 'input',
-            message: 'what is your read me title?',
-            name: 'title'
-        },
-        {
-            type: 'input',
-            message: 'description',
-            name: 'description'
-        },
-        {
-            type: 'checkbox',
-            message: 'table of contents',
-            name: 'table',
-            choices: ['title', 'description', 'table of contents', 'installation', 'license', 'contributing', 'tests', 'questions']
-        },
-        {
-            type: 'input',
-            message: 'installation instructions',
-            name: 'installation'
-        },
-        {
-            type: 'list',
-            message: 'What license are you using?',
-            name: 'license',
-            choices: ['CCO', 'MIT', 'apache', 'MPL', 'GPL', 'AGPL', 'JRL', 'AFPL', 'Proprietary license', 'Trade secrets']
-        },
-        {
-            type: 'input',
-            message: 'contributing',
-            name: 'contributing'
-        },
-        {
-            type: 'input',
-            message: 'tests',
-            name: 'tests'
-        },
-        {
-            type: 'input',
-            message: 'what is your github url?',
-            name: 'github'
-        },
-        {
-            type: 'input',
-            message: 'what is your email?',
-            name: 'email'
-        }
-    ]
-)
 
-.then((response) => {
-    var title = response.title;
-    console.log(title);
-    var description = response.description;
-    var tableOfContents = response.table;
-    var installation = response.installation;
-    var license = response.installation;
-    var contributing = response.contributing;
-    var tests = response.tests;
-    var github = response.github;
-    var email = response.email;
-});
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function init() {
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) => {
+            if (err)
+                console.log(err);
+            else {
+                console.log("File written successfully\n");
+            }
+        });
+    }
 
-// TODO: Create a function to initialize app
-function init() {}
+    inquirer.prompt(
+        [{
+                type: 'input',
+                message: 'what is your read me title?',
+                name: 'title'
+            },
+            {
+                type: 'input',
+                message: 'description',
+                name: 'description'
+            },
+            {
+                type: 'checkbox',
+                message: 'table of contents',
+                name: 'table',
+                choices: ['title', 'description', 'table of contents', 'installation', 'license', 'contributing', 'tests', 'questions']
+            },
+            {
+                type: 'input',
+                message: 'installation instructions',
+                name: 'installation'
+            },
+            {
+                type: 'list',
+                message: 'What license are you using?',
+                name: 'license',
+                choices: ['CCO', 'MIT', 'apache', 'MPL', 'GPL', 'AGPL', 'JRL', 'AFPL', 'Proprietary license', 'Trade secrets']
+            },
+            {
+                type: 'input',
+                message: 'contributing',
+                name: 'contributing'
+            },
+            {
+                type: 'input',
+                message: 'tests',
+                name: 'tests'
+            },
+            {
+                type: 'input',
+                message: 'what is your github url?',
+                name: 'github'
+            },
+            {
+                type: 'input',
+                message: 'what is your email?',
+                name: 'email'
+            }
+        ]
+    )
 
-// Function call to initialize app
+    .then((response) => {
+        const generatedReadMe = generateMarkDown(response);
+        console.log(generatedReadMe);
+        writeToFile("generatedREADME.md", generatedReadMe);
+    });
+};
+
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,6 @@
+// var index = require('../index')
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -12,12 +15,53 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 
-
 function generateMarkdown(data) {
-    console.log(data.title);
-    return `# ${data.title}
 
-     `;
+    var title = data.title;
+    var description = data.description;
+    var table = data.table;
+    var install = data.installation;
+    var license = data.license;
+    var contributing = data.contributing;
+    var tests = data.tests;
+    var github = data.github;
+    var email = data.email;
+
+    return `# ${title}
+    // ## Description
+    // ${description}
+    // Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+    // - What was your motivation?
+    // - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+    // - What problem does it solve?
+    // - What did you learn?
+    // ##Table of Contents 
+    ${table}
+    // ## Installation Instructions
+    // ${install}
+    // What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+    // ## Usage
+    // Provide instructions and examples for use. Include screenshots as needed.
+    // To add a screenshot, create a folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+    // ## Credits
+    // ${contributing}
+    // List your collaborators, if any, with links to their GitHub profiles.
+    // If you used any third-party asset that requir attribution, list the creators with links to their primary web presence in this section.
+    // If you followed tutorials, include links to those here as well.
+    // ## License
+    // The last section of a high-quality README file is the ${license}. This lets other developers know what they can and cannot do with your project.
+    // ---
+    // ## Badges
+    // ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+    ## Questions
+    ${github}
+    ${email}
+    ##Tests
+    ${tests}
+    `
+
+
+
 }
-console.log(generateMarkdown);
+
 module.exports = generateMarkdown;
