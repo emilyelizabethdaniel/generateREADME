f0bf62d3d3382f68066d92da7463d8eb5f9789bd
Basquiat <ememdaniel98@gmail.com>
2021-10-12 21:39:36 -0500

    working on license functions, got read me to generate

---
+++ Develop/generatedREADME.md
@@ -0,0 +1,35 @@
+# my title
+## Badges
+![image description or alt text](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
+    
+## Description
+my description
+Provide a short description explaining the what, why, and how of your project.
+   
+##Table of Contents 
+title,description,table of contents,installation,license,contributing,tests,questions
+    
+## Installation Instructions
+intstall it
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+   
+## Usage
+Provide instructions and examples for use. Include screenshots as needed.
+To add a screenshot, create a folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+   
+## Credits
+timothy mon petit amie
+List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party asset that requir attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well.
+## License
+you chose license MIT
+The last section of a high-quality README file is the MIT.
+
+This lets other developers know what they can and cannot do with your project.
+   
+## Questions
+.com
+.com
+##Tests
+none
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,9 +1,8 @@
-// var index = require('../index')
-
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    return `you chose license ${license}`;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -28,40 +27,41 @@ function generateMarkdown(data) {
     var email = data.email;
 
     return `# ${title}
-    // ## Description
-    // ${description}
-    // Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-    // - What was your motivation?
-    // - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-    // - What problem does it solve?
-    // - What did you learn?
-    // ##Table of Contents 
-    ${table}
-    // ## Installation Instructions
-    // ${install}
-    // What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-    // ## Usage
-    // Provide instructions and examples for use. Include screenshots as needed.
-    // To add a screenshot, create a folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-    // ## Credits
-    // ${contributing}
-    // List your collaborators, if any, with links to their GitHub profiles.
-    // If you used any third-party asset that requir attribution, list the creators with links to their primary web presence in this section.
-    // If you followed tutorials, include links to those here as well.
-    // ## License
-    // The last section of a high-quality README file is the ${license}. This lets other developers know what they can and cannot do with your project.
-    // ---
-    // ## Badges
-    // ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-    ## Questions
-    ${github}
-    ${email}
-    ##Tests
-    ${tests}
-    `
+## Badges
+![image description or alt text](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
+    
+## Description
+${description}
+Provide a short description explaining the what, why, and how of your project.
+   
+##Table of Contents 
+${table}
+    
+## Installation Instructions
+${install}
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+   
+## Usage
+Provide instructions and examples for use. Include screenshots as needed.
+To add a screenshot, create a folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+   
+## Credits
+${contributing}
+List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party asset that requir attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well.
+## License
+${renderLicenseBadge(license)}
+The last section of a high-quality README file is the ${license}.
 
-
-
+This lets other developers know what they can and cannot do with your project.
+   
+## Questions
+${github}
+${email}
+##Tests
+${tests}
+`
 }
 
 module.exports = generateMarkdown;
